4cd445aae33ea39534b7316f842e6bdeb4abf0fb
shinoki <endernasha85@gmail.com>
2023-08-04 21:24:47 +0300

    nest chat's resolve dependencies fixes

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,21 +1,22 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-// import { FileService } from '../file/file.service';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { Chat, ChatSchema } from './schemas/chat.schema';
 import { UsersModule } from '../users/users.module';
+import { FileService } from '../file/file.service';
+
+// import { FileService } from '../file/file.service';
 // import { TokenModule } from '../token/token.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
         UsersModule,
-        // TokenModule,
     ],
     controllers: [ChatController],
-    providers: [ChatService],
+    providers: [ChatService, FileService],
     exports: [ChatService],
 })
 export class ChatModule {}
